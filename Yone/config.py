22a7d4ad2f6328b3c55348d1cac53f6cc1b2facf
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-01-19 12:44:46 +0530

    Yone New Update Bot

---
+++ Yone/config.py
@@ -0,0 +1,41 @@
+
+class Config(object):
+    LOGGER = True
+
+    API_ID = "12227067 " 
+    API_HASH = "b463bedd791aa733ae2297e6520302fe"
+    TOKEN = ""  
+    OWNER_ID = 5360305806 
+    SULTAN = 2105971379
+    CHAT = "+jCS-YsVBVEE3NjQ1"
+    SUPPORT_CHAT = "AM_YTSUPPORT"
+    START_IMG = "https://graph.org/file/41d20249ffdb1151b8870.jpg"
+    EVENT_LOGS = ("-1001908711819")
+    JOIN_LOGGER = ("-1001841879487")
+    MONGO_DB_URI= "mongodb+srv://DevuMusicBot:CBPNyUTcJCD1P8ZD@devumusicbot.sdqazcu.mongodb.net/?retryWrites=true&w=majority"
+    DATABASE_URL = "postgres://yone:Kushal55@yone.cirqmtrbghab.us-east-1.rds.amazonaws.com:5432/yone" 
+    CASH_API_KEY = ("PNNU99H3W9KDLKVM"  )
+    TIME_API_KEY = "9HK7J0H25AKQ"
+    BL_CHATS = []  
+    DRAGONS = [] 
+    DEV_USERS = []  
+    DEMONS = []  
+    TIGERS = []  
+    WOLVES = []  
+    ALLOW_CHATS = True
+    ALLOW_EXCL = True
+    DEL_CMDS = True
+    INFOPIC = True
+    LOAD = []
+    NO_LOAD = []
+    STRICT_GBAN = True
+    TEMP_DOWNLOAD_DIRECTORY = "./"
+    WORKERS = 8
+    
+
+class Production(Config):
+    LOGGER = True
+
+
+class Development(Config):
+    LOGGER = True
